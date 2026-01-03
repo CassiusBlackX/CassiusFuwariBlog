bb899c0be15a9ac3eb9715961d27122e12dbd273
CassiusBlackX <cassiusx@qq.com>
2026-01-03 17:59:02 +0800

    update site addr

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.cassiusblack.top/",
+	site: "https://blog.cassiusblack.xyz/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
